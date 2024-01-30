e55ebfd942d152fa6056503dc264fbab0fa0a6f1
Semion <semionchi@gmail.com>
2024-01-30 21:06:40 +0200

    db connetion setup

--- actions/login.ts
+++ actions/login.ts
@@ -1,11 +1,32 @@
 "use server";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
 import * as z from "zod";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validateFields = LoginSchema.safeParse(values);
   if (!validateFields.success) {
     return { error: "invalid fields!" };
   }
-  return { success: "Email sent" };
+  const { email, password } = validateFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    //TODO
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid cradentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
---
+++ routes.ts
@@ -0,0 +1,15 @@
+/**
+ * Public routes
+ */
+export const publicRoutes = ["/"];
+/**
+ * Auth routes
+ */
+
+export const authRoutes = ["/login", "/register"];
+/**
+ * Prefix for API auth routes
+ */
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
